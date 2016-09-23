5acff5d54a121fb45a4b149a9d58e326ca3ea439
Kgotso <mahipsafet@gmail.com>
2016-09-23 19:36:35 +0200

    Initial work on turret AI, lock on to target

---
+++ Assets/TurretAI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretAI : MonoBehaviour {
+	private Transform target;
+
+	public float range = 20f;
+	public string enemyTag = "CarPlayer";
+
+	public float turnSpeed = 2.5f;
+	public Transform pivot;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("UpdateTarget", 0f, 0.5f);
+	}
+
+	void UpdateTarget(){
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag (enemyTag);
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+		foreach (GameObject enemy in enemies) {
+			float distanceToEnemy = Vector3.Distance (transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance) {
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+		if (nearestEnemy != null &&	shortestDistance <= range) {
+			target = nearestEnemy.transform;
+		} else {
+			target = null;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation (dir);
+		Vector3 rotation = Quaternion.Lerp(pivot.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		pivot.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+	}
+
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position, range);
+	}
+}

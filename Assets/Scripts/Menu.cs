3e4bd974c740a9c327fa7b15432ccde9a3fe38da
ResidentZealoT <resident@hotmail.co.za>
2016-10-29 18:18:23 +0200

    Worked on Menu Scene

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,132 +6,73 @@ using UnityEngine.Networking;
 
 public class Menu : NetworkBehaviour 
 {
-
-    public GameObject server;
-    NetworkManagerHUD hudManager;
-
     // Canvas objects
-    public Canvas ModeCanvas;
 	public Canvas SelectionCanvas;
-    public Canvas LobbyCanvas; 
-	public Image SplashScreen;
 
     // vehicel objetcs
     public GameObject ship;
     public GameObject car;
     public RectTransform vehicles;
-
+	Renderer[] SelectionRenderers;
     private int scrollDirection = 0;
     public int rightTarget = 0;
     public int leftTarget = -100;
 
 	// Boolean to check if game is networked
 	private bool isMultiplayer;
-	// counter to track game time
-	private float counter = 0;
-
-    Renderer[] SelectionRenderers;
 
 
     // On Awake Menu Selection is disabled
     void Awake()
 	{
-        scrollDirection = 0;
+        SelectionCanvas.enabled = true;
+		ship.SetActive(false);
+		car.SetActive(false);
 
-        LobbyCanvas.enabled = false;
-        hudManager = server.GetComponent<NetworkManagerHUD>();
-        hudManager.showGUI = false;
-    
-        SelectionCanvas.enabled = false;
+		// car selection controls
         SelectionRenderers = SelectionCanvas.GetComponentsInChildren<Renderer>();
+		scrollDirection = 0;
         foreach (Renderer r in SelectionRenderers)
         {
             r.enabled = false;
         }
     }
 
-    // update counter and handle splash screen
+    // car rotations
     void Update()
 	{
-        // Rotate the object around its local X axis at 1 degree per second
-        ship.transform.Rotate(Vector3.up * Time.deltaTime*100);
-        car.transform.Rotate(Vector3.up * Time.deltaTime*100);
+		// Rotate the object around its local X axis at 1 degree per second
+		ship.transform.Rotate (Vector3.up * Time.deltaTime * 100);
+		car.transform.Rotate (Vector3.up * Time.deltaTime * 100);
 
-        if (scrollDirection !=0) // left/right button pressed
-        {
-            vehicles.transform.Translate(vehicles.transform.right * Time.deltaTime * 200 * scrollDirection); // move the vehicles in direction (1 or -1) 
+		// car selection controls
+		if (scrollDirection != 0) { // left/right button pressed
+			vehicles.transform.Translate (vehicles.transform.right * Time.deltaTime * 200 * scrollDirection); // move the vehicles in direction (1 or -1) 
             
-            if (vehicles.transform.position.x > rightTarget) // ship in center (move all the way right)
-            {
-                scrollDirection = 0; // stop vehicles moving
-                vehicles.transform.position = new Vector3(rightTarget, 0, 0); // stick vehicles to the far right
-            }
-            else if(vehicles.transform.position.x < leftTarget) // car in the center (moved all the way to the left)
-            {
-                scrollDirection = 0;// stop vehicles moving
-                vehicles.transform.position = new Vector3(leftTarget, 0, 0);// stick vehicles to the far left
-            }
-        }
-
-        counter += Time.deltaTime;
-		if (counter >= 2) 
-		{
-			SplashScreen.enabled = false;
+			if (vehicles.transform.position.x > rightTarget) { // ship in center (move all the way right)
+				scrollDirection = 0; // stop vehicles moving
+				vehicles.transform.position = new Vector3 (rightTarget, 0, 0); // stick vehicles to the far right
+			} else if (vehicles.transform.position.x < leftTarget) { // car in the center (moved all the way to the left)
+				scrollDirection = 0;// stop vehicles moving
+				vehicles.transform.position = new Vector3 (leftTarget, 0, 0);// stick vehicles to the far left
+			}
 		}
 	}
 
-	// Select Multiplayer Mode
-	public void LobbySelection()
+	// Start a game singleplayer game
+	public void LoadOnSingle()
 	{
-        LobbyCanvas.enabled = true;
-        hudManager.showGUI = true;
-        SelectionCanvas.enabled = false;
-        foreach (Renderer r in SelectionRenderers)
-        {
-            r.enabled = false;
-        }
-        ModeCanvas.enabled = false;
-		isMultiplayer = true;
-	}
-
-    public void MultiplayerSelection()
-    {
-        LobbyCanvas.enabled = false;
-        hudManager.showGUI = false;
-        SelectionCanvas.enabled = true;
-        foreach (Renderer r in SelectionRenderers)
-        {
-            r.enabled = true;
-        }
-    }
-
-	// Select Solo Mode
-	public void SoloSelectionOn()
-	{
-		SelectionCanvas.enabled = true;
-        // rendering all children of this gameobject to show the vehicles
-        foreach (Renderer r in SelectionRenderers)
-        {
-            r.enabled = true;
-        }
-        ModeCanvas.enabled = false;
-		isMultiplayer = false;
-	}
-
-	// Start a game, determine if mulitplayer or solo
-	public void LoadOn()
-	{
-
-		if (isMultiplayer) 
-		{
-            SceneManager.LoadScene (3);
-        }
-		else if (!isMultiplayer) 
-		{
-			SceneManager.LoadScene (1);
-		}
+		SceneManager.LoadScene (1);
 	}
     
+	// start a multiplayer game
+	public void LoadOnMulti()
+	{
+		isMultiplayer = true;
+		SceneManager.LoadScene (3);
+	}
+
+	// handle car rotations
     public void scrollLeftClick() // left selection button selected
     {
         scrollDirection = -1;

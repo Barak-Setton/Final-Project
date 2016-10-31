616376e5dce758ab5d43ceba251dafbe1b6e4095
ResidentZealoT <resident@hotmail.co.za>
2016-10-31 04:28:56 +0200

    Did a lot of work to menu scene

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -14,29 +14,33 @@ public class Menu : NetworkBehaviour
     public GameObject car;
     public RectTransform vehicles;
 	Renderer[] SelectionRenderers;
-    private int scrollDirection = 0;
     public int rightTarget = 0;
     public int leftTarget = -100;
 
 	// Boolean to check if game is networked
-	private bool isMultiplayer;
-
+	public bool isMultiplayer;
+	public bool isShip;
 
     // On Awake Menu Selection is disabled
     void Awake()
 	{
         SelectionCanvas.enabled = true;
-		ship.SetActive(false);
-		car.SetActive(false);
 
 		// car selection controls
         SelectionRenderers = SelectionCanvas.GetComponentsInChildren<Renderer>();
-		scrollDirection = 0;
         foreach (Renderer r in SelectionRenderers)
         {
             r.enabled = false;
         }
     }
+
+	void Start()
+	{
+		ship.SetActive(false);
+		car.SetActive(false);
+		scrollLeftClick ();
+		isShip = true;
+	}
 
     // car rotations
     void Update()
@@ -44,19 +48,6 @@ public class Menu : NetworkBehaviour
 		// Rotate the object around its local X axis at 1 degree per second
 		ship.transform.Rotate (Vector3.up * Time.deltaTime * 100);
 		car.transform.Rotate (Vector3.up * Time.deltaTime * 100);
-
-		// car selection controls
-		if (scrollDirection != 0) { // left/right button pressed
-			vehicles.transform.Translate (vehicles.transform.right * Time.deltaTime * 200 * scrollDirection); // move the vehicles in direction (1 or -1) 
-            
-			if (vehicles.transform.position.x > rightTarget) { // ship in center (move all the way right)
-				scrollDirection = 0; // stop vehicles moving
-				vehicles.transform.position = new Vector3 (rightTarget, 0, 0); // stick vehicles to the far right
-			} else if (vehicles.transform.position.x < leftTarget) { // car in the center (moved all the way to the left)
-				scrollDirection = 0;// stop vehicles moving
-				vehicles.transform.position = new Vector3 (leftTarget, 0, 0);// stick vehicles to the far left
-			}
-		}
 	}
 
 	// Start a game singleplayer game
@@ -75,11 +66,31 @@ public class Menu : NetworkBehaviour
 	// handle car rotations
     public void scrollLeftClick() // left selection button selected
     {
-        scrollDirection = -1;
+		if (car.activeInHierarchy) {
+			ship.SetActive (true);
+			car.SetActive (false);
+			isShip = true;
+		} else if (ship.activeInHierarchy) {
+			ship.SetActive (false);
+			car.SetActive (true);
+			isShip = false;
+		} else {
+			ship.SetActive (true);
+			car.SetActive (false);
+		}
     }
     public void scrollRightClick() // right selection button selected
     {
-        scrollDirection = 1;
+		if (car.activeInHierarchy) {
+			ship.SetActive (true);
+			car.SetActive (false);
+			isShip = true;
+		}
+		else if (ship.activeInHierarchy) {
+			ship.SetActive (false);
+			car.SetActive (true);
+			isShip = false;
+		}
     }
 
 }

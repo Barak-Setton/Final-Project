548dfacfc07490cf45778f709c40963c2165a5aa
Kgotso <mahipsafet@gmail.com>
2016-11-13 03:20:11 +0200

    added a loss screen.

---
+++ Assets/explodingPlanet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class explodingPlanet : MonoBehaviour {
+
+	public int explosionTime = 5;
+	public float elapsedTime = 0;
+	public int newSceneTime = 10;
+	public ParticleSystem explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsedTime += Time.deltaTime;
+
+		if (elapsedTime == explosionTime) {
+			explosion.Play ();
+			Destroy (this);
+
+		}
+
+		if (elapsedTime >= newSceneTime) {
+			SceneManager.LoadScene (5);
+		}
+			
+	}
+}

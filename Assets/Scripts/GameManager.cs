db8ed8bac2f053d3821f4b02ddf7502dea00188c
ResidentZealoT <resident@hotmail.co.za>
2016-11-13 14:28:21 +0200

    changes to vehicle prefabss & gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,7 +15,6 @@ public class GameManager : MonoBehaviour {
 	// spawnLocations
     public Transform spawnPointPlayer1;
     public Transform spawnPointPlayer2;
-
     public GameObject spawnPlane;
 
 	// players
@@ -25,7 +24,6 @@ public class GameManager : MonoBehaviour {
 	//hud elements
     public GameObject digitalSpeed;
     public GameObject analogSpeed;
-
     public GameObject powerBar;
 
     private GameObject transferData;
@@ -58,56 +56,7 @@ public class GameManager : MonoBehaviour {
 
 		//set the instance of this object
 		managerController = this;
-
 		state = StateType.START;
-		// TEMP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		//Handle canvas Init
-		gameOverCanvas.enabled = false;
-        if (TransferData.instance.multiplayerCheck) // Multiplayer
-        {
-
-        }
-        else { 
-            if (TransferData.instance.shipID) // Singleplayer
-            {
-                // instantiating the Ship and renaming
-                player1 = ship;
-                player1 = (GameObject)Instantiate(player1, spawnPointPlayer1.position, Quaternion.identity);
-                player1.name = "AirshipC";
-                digitalSpeed.GetComponent<digitalSpeedometer>().vehical = player1;
-                analogSpeed.SetActive(false);
-
-                // activating AI
-                player2 = carAI;
-                player2.SetActive(true);
-            }
-            else
-            {
-                // instantiating the car and renaming
-                player1 = car;
-                player1 = (GameObject) Instantiate(player1, spawnPointPlayer1.position, Quaternion.identity);
-                player1.name = "groundCar";
-                analogSpeed.GetComponent<analogSpeedometer>().vehical = player1;
-                digitalSpeed.SetActive(false);
-
-                // activating AI
-                player2 = shipAI;
-                player2.SetActive(true);
-
-            }
-
-            // set powerbar
-            powerBar.GetComponent<PowerBar>().setPlayer(player1);
-           // print(player1);
-
-            // setting smooth camera target
-            smoothCamera.GetComponent<SmoothFollowCamera>().target = player1.GetComponent<Transform>();
-
-            // setting spawnplane info
-            spawnPlane.GetComponent<RespawnTrigger>().players[1] = player1;
-            spawnPlane.GetComponent<RespawnTrigger>().players[0] = player2;
-
-        }
     }
 	
 	// Update is called once per frame
@@ -115,21 +64,71 @@ public class GameManager : MonoBehaviour {
 	
 	//switch statement acts as determined by statelist
 		switch (state) {
+
 		case StateType.START:
-			player1.SetActive(true);
-            player2.SetActive(true);
+			// Choose Canvas
+			this.SetState (StateType.GAMEPLAY);
 			gameOverCanvas.enabled = false;
+
+			if (TransferData.instance.multiplayerCheck) { // Multiplayer
+
+			} else { 
+				if (TransferData.instance.shipID) { // Singleplayer
+					// instantiating the Ship and renaming
+					player1 = ship;
+					player1 = (GameObject)Instantiate (player1, spawnPointPlayer1.position, Quaternion.identity);
+					player1.name = "AirshipC";
+					digitalSpeed.GetComponent<digitalSpeedometer> ().vehical = player1;
+					analogSpeed.SetActive (false);
+
+					// activating AI
+					player2 = carAI;
+					player2.SetActive (true);
+				} else if (!TransferData.instance.shipID) {
+					// instantiating the car and renaming
+					player1 = car;
+					player1 = (GameObject)Instantiate (player1, spawnPointPlayer1.position, Quaternion.identity);
+					player1.name = "groundCar";
+					print("-1111");
+					analogSpeed.GetComponent<analogSpeedometer>().vehical = player1;
+					print("000");
+					digitalSpeed.SetActive (false);
+					print("1111");
+					// activating AI
+					player2 = shipAI;
+					player2.SetActive (true);
+					print("2");
+
+				}
+
+				// set powerbar
+				powerBar.GetComponent<PowerBar> ().setPlayer (player1);
+
+				// setting smooth camera target
+				smoothCamera.GetComponent<SmoothFollowCamera> ().target = player1.GetComponent<Transform> ();
+
+				// setting spawnplane info
+				spawnPlane.GetComponent<RespawnTrigger> ().players [1] = player1;
+				spawnPlane.GetComponent<RespawnTrigger> ().players [0] = player2;
+
+			}
 			break;
+
 		case StateType.GAMEPLAY:
+			
 			gameOverCanvas.enabled = false;
 			break;
+
 		case StateType.ENDGAME:
+			
 			player1.SetActive (false);
 			player2.SetActive (false);
 			counter = 0;
 			gameOverCanvas.enabled = true;
 			break;
+
 		default:
+			
 			break;
 		}
 	}

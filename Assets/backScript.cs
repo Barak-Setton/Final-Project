9b2d6aff2db31d0a8275f4ee26e10866dfac9768
ResidentZealoT <resident@hotmail.co.za>
2016-11-13 17:53:55 +0200

    network Hud modifications

---
+++ Assets/backScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class backScript : MonoBehaviour {
+
+	public void LoadOnBack()
+	{
+		SceneManager.LoadScene (0);
+	}
+}

effc4288dd4b92a5765e9f7972ee222c29efe8d8
Barak-Setton <setton.b@gmail.com>
2016-11-13 17:03:48 +0200

    updated the HUD with all network scripts

---
+++ Assets/Scripts/PowerBarNetwork.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PowerBarNetwork : NetworkBehaviour
+{
+
+    public GameObject player1;
+
+    private PowerbarTracker powerBar;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (player1.GetComponent<PowerbarTracker>().hasPower())
+        {
+            GetComponent<Text>().text = "Power: " + player1.GetComponent<PowerbarTracker>().getPower();
+        }
+
+    }
+
+    public void setPlayer(GameObject player)
+    {
+        this.player1 = player;
+    }
+}

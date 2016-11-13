89e8843bc47bf3415c8f460107436d89f6f68655
Kgotso <mahipsafet@gmail.com>
2016-11-13 14:36:26 +0200

    respawn Trigger fix.

--- Assets/Scripts/RespawnTrigger.cs
+++ Assets/Scripts/RespawnTrigger.cs
@@ -3,22 +3,18 @@ using System.Collections;
 
 public class RespawnTrigger : MonoBehaviour {
 
-	public GameObject[] players;
+
 	public Transform spawnPoint;
-	public Transform spawnPoint2;
 
 	// respawn based on tag
 	public void OnTriggerEnter(Collider col) {
-		spawnPoint = col.gameObject.GetComponentInChildren<Dumm
+		spawnPoint = col.gameObject.GetComponentInChildren<Dummy> ().GetComponent<Transform> ();
 		if (col.tag == "Player") {
 			//respawn 
 			col.gameObject.transform.position = spawnPoint.position;
 			col.gameObject.transform.rotation = spawnPoint.rotation;
 
-		} else if (col.tag == "CarPlayer") {
-			players [1].transform.position = spawnPoint2.position;
-			players [1].transform.rotation = spawnPoint2.rotation;
-		}
+		} 
 
 	}
 }

7853b1d98cdaf4d40979de18199b6b93a85e1936
ResidentZealoT <resident@hotmail.co.za>
2016-11-15 00:47:15 +0200

    finished endgame scene

--- Assets/explodingPlanet.cs
+++ Assets/explodingPlanet.cs
@@ -3,27 +3,41 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 public class explodingPlanet : MonoBehaviour {
 
+	public float awwTime;
+	public float yayTime;
+	bool yayed = false;
+	bool awwed = false;
 	public float destructionTime;
+	public float celebrationTime;
 	public float printerTime;
 	public int explosionTime;
 	public float explosionDone;
+	public float celebrationDone;
 	public float elapsedTime = 0;
 	public int newSceneTime;
 	public float rotateSpeed = 25.0f;
 	public ParticleSystem explosion;
+	public ParticleSystem celebration;
 	public GameObject planet;
 	public GameObject printer;
 	bool exploded = false;
 	bool sounded = false;
+	bool celebrate = false;
 	public GameObject particleParent;
-	AudioSource explosionSound;
+	public GameObject celebrationParent;
+	//AudioSource explosionSound;
+	AudioSource aww;
+	AudioSource yay;
+	AudioSource[] sounds;
 
 	public GameObject text1;
 	public GameObject text2;
 
 	// Use this for initialization
 	void Start () {
-		explosionSound = GetComponent<AudioSource> ();
+		sounds = GetComponents<AudioSource> ();
+		aww = sounds[0];
+		yay = sounds [1];
 		text1.SetActive (true);
 		text2.SetActive (false);
 		printer.SetActive (false);
@@ -38,14 +52,17 @@ public class explodingPlanet : MonoBehaviour {
 
 		if (elapsedTime >= destructionTime && !sounded) {
 			planet.SetActive(false);
-			explosionSound.Play ();
 			sounded = true;
 		}
 
 		if (elapsedTime >= explosionTime && !exploded) {
 			explosion.Play();
 			exploded = true;
-			//explosion.Stop ();
+		}
+
+		if (elapsedTime >= awwTime && !awwed) {
+			aww.Play ();
+			awwed = true;
 		}
 
 		if (elapsedTime > explosionDone) {
@@ -56,6 +73,20 @@ public class explodingPlanet : MonoBehaviour {
 
 		if (elapsedTime > printerTime) {
 			printer.SetActive (true);
+		}
+
+		if (elapsedTime >= celebrationTime && !celebrate) {
+			celebration.Play();
+			celebrate = true;
+		}
+
+		if (elapsedTime >= yayTime && !yayed) {
+			yay.Play ();
+			yayed = true;
+		}
+
+		if (elapsedTime > celebrationDone) {
+			celebrationParent.SetActive(false);
 		}
 
 		if (elapsedTime >= newSceneTime) {

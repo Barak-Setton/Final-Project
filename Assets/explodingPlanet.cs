f03545ccc17f8421f8e5131d5b1f1aa36d754348
ResidentZealoT <resident@hotmail.co.za>
2016-11-14 20:41:11 +0200

    working on end game scene

--- Assets/explodingPlanet.cs
+++ Assets/explodingPlanet.cs
@@ -6,6 +6,7 @@ public class explodingPlanet : MonoBehaviour {
 	public int explosionTime = 5;
 	public float elapsedTime = 0;
 	public int newSceneTime = 10;
+	public float rotateSpeed = 25.0f;
 	public ParticleSystem explosion;
 	public GameObject planet;
 
@@ -18,9 +19,12 @@ public class explodingPlanet : MonoBehaviour {
 	void Update () {
 		elapsedTime += Time.deltaTime;
 
+		// rotate the planet
+		transform.Rotate(transform.up * rotateSpeed * Time.deltaTime);
+
 		if (elapsedTime == explosionTime) {
 			explosion.Play ();
-			Destroy (planet);
+			Destroy (this);
 
 		}
 

451e38d28769267ca794e494512ef43d2c023e52
Kgotso <mahipsafet@gmail.com>
2016-11-14 15:34:05 +0200

    spawnpoint position updater.

--- Assets/Dummy.cs
+++ Assets/Dummy.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class Dummy : MonoBehaviour {
-
+	public Vector3 position;
+	public Quaternion rotation;
+	private Transform parentTransform;
 	// Use this for initialization
 	void Start () {
-	
+		parentTransform = GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		parentTransform.position = position;
+		parentTransform.rotation = rotation;
 	}
 }
--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,17 +3,21 @@ using System.Collections;
 using UnityEngine.Networking;
 public class Checkpoint : NetworkBehaviour {
 
-	public Transform spawnPoint;
+	public Dummy spawnPoint;
 
 	public void OnTriggerEnter(Collider col)
 	{
 		if (TransferData.instance.multiplayerCheck && !isLocalPlayer)
 			return;
 		GameManager.managerController.counter++;
-		spawnPoint = col.gameObject.GetComponentInChildren<Dummy> ().GetComponent<Transform> ();
+		spawnPoint = col.gameObject.GetComponentInChildren<Dummy> ();
 		if (col.tag == "Player") {
 			spawnPoint.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-			spawnPoint.transform.rotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+			spawnPoint.rotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
 		} 
+	}
+
+	public void update(){
+
 	}
 }

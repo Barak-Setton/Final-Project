e493703dee0fb359848f05062a6ca9cf3930ca25
ResidentZealoT <resident@hotmail.co.za>
2016-11-14 14:47:39 +0200

    Added depricated code YAY

--- Assets/enabler.cs
+++ Assets/enabler.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class enabler : MonoBehaviour {
 	public GameObject manager;
 	public GameObject respawnPlane;
-	public GameObject[] checkpoints;
+	public GameObject checkpointContainer;
 
 	// Use this for initialization
 	void Start () {
 		manager.SetActive(true);
 		respawnPlane.SetActive(true);
-		checkpoints = GameObject.FindGameObjectsWithTag ("Checkpoint");
-		foreach (GameObject checkpoint in checkpoints) {
-			checkpoint.SetActive (true);
-		}
+		checkpointContainer.SetActiveRecursively (true);
+		//checkpoints = this.ch ("Checkpoint");
+		//print ("checkpoint" + checkpoints[0].name);
+		//foreach (GameObject checkpoint in checkpointContainer) {
+		//	checkpoint.set (true);
+		//	print ("checkpoint" + checkpoint.name);
+		//}
 	}
-	
+
 
 }

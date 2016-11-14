f595d370a45f591899118d43548cd38ac0be38f4
ResidentZealoT <resident@hotmail.co.za>
2016-11-14 17:28:06 +0200

    fixed tags

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,7 +6,7 @@ public class GameOver : MonoBehaviour {
 	// when a player crosses the finish line set the gamestate to ENDGAME
 	public void OnTriggerEnter(Collider col)
 	{
-		if ((col.tag == "ShipPlayer" || col.tag == "CarPlayer") && GameManager.managerController.counter > 20) {
+		if ((col.tag == "Player" || col.tag == "Player") && GameManager.managerController.counter > 20) {
 			GameManager.managerController.SetState (GameManager.StateType.ENDGAME);
 		}
 	}

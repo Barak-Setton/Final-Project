6034cff4ce489742940734d818cfa2e59eed4ec0
ResidentZealoT <resident@hotmail.co.za>
2016-11-14 23:16:26 +0200

    endgame scene

--- Assets/explodingPlanet.cs
+++ Assets/explodingPlanet.cs
@@ -3,16 +3,30 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 public class explodingPlanet : MonoBehaviour {
 
-	public int explosionTime = 5;
+	public float destructionTime;
+	public float printerTime;
+	public int explosionTime;
+	public float explosionDone;
 	public float elapsedTime = 0;
-	public int newSceneTime = 10;
+	public int newSceneTime;
 	public float rotateSpeed = 25.0f;
 	public ParticleSystem explosion;
 	public GameObject planet;
+	public GameObject printer;
+	bool exploded = false;
+	bool sounded = false;
+	public GameObject particleParent;
+	AudioSource explosionSound;
+
+	public GameObject text1;
+	public GameObject text2;
 
 	// Use this for initialization
 	void Start () {
-	
+		explosionSound = GetComponent<AudioSource> ();
+		text1.SetActive (true);
+		text2.SetActive (false);
+		printer.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -20,16 +34,32 @@ public class explodingPlanet : MonoBehaviour {
 		elapsedTime += Time.deltaTime;
 
 		// rotate the planet
-		transform.Rotate(transform.up * rotateSpeed * Time.deltaTime);
+		planet.transform.Rotate(planet.transform.up * rotateSpeed * Time.deltaTime);
 
-		if (elapsedTime == explosionTime) {
-			explosion.Play ();
-			Destroy (this);
+		if (elapsedTime >= destructionTime && !sounded) {
+			planet.SetActive(false);
+			explosionSound.Play ();
+			sounded = true;
+		}
 
+		if (elapsedTime >= explosionTime && !exploded) {
+			explosion.Play();
+			exploded = true;
+			//explosion.Stop ();
+		}
+
+		if (elapsedTime > explosionDone) {
+			particleParent.SetActive(false);
+			text2.SetActive (true);
+			text1.SetActive (false);
+		}
+
+		if (elapsedTime > printerTime) {
+			printer.SetActive (true);
 		}
 
 		if (elapsedTime >= newSceneTime) {
-			SceneManager.LoadScene (5);
+			SceneManager.LoadScene (0);
 		}
 			
 	}

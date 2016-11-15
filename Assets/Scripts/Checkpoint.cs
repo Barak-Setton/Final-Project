509735e78b4fa62985488cc6159750e5ab203118
ResidentZealoT <resident@hotmail.co.za>
2016-11-15 07:27:32 +0200

    small checkpoint fix

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -7,10 +7,9 @@ public class Checkpoint : MonoBehaviour {
 
 	public void OnTriggerEnter(Collider col)
 	{
-		
-		GameManager.managerController.counter++;
 		spawnPoint = col.gameObject.GetComponentInChildren<SpawnpointScript> ();
 		if (col.tag == "Vehicel") {
+			GameManager.managerController.counter++;
 			spawnPoint.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 			spawnPoint.rotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
 		} 

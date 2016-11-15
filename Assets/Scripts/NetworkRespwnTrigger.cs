679ff61ec92fd983f804319ef15033bf57d455cb
Barak-Setton <setton.b@gmail.com>
2016-11-15 06:24:39 +0200

    parse error fixed

--- Assets/Scripts/NetworkRespwnTrigger.cs
+++ Assets/Scripts/NetworkRespwnTrigger.cs
@@ -6,21 +6,23 @@ public class NetworkRespawnTrigger : NetworkBehaviour {
 
 	public NetworkSpawnpointScript spawnPoint;
 
-	// respawn based on tag
-	public void OnTriggerEnter(Collider col) {
-		if (TransferData.instance.multiplayerCheck && !isLocalPlayer)
-			return;
-		spawnPoint = col.gameObject.GetComponentInChildren<NetworkSpawnpointScript> ();
-		if (col.tag == "Vehicel") {
-			//respawn 
-			col.gameObject.transform.position = spawnPoint.position;
-			col.gameObject.transform.rotation = spawnPoint.rotation;
-			col.gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+    // respawn based on tag
+    public void OnTriggerEnter(Collider col)
+    {
+        if (TransferData.instance.multiplayerCheck && !isLocalPlayer)
+            return;
+        spawnPoint = col.gameObject.GetComponentInChildren<NetworkSpawnpointScript>();
+        if (col.tag == "Vehicel")
+        {
+            //respawn 
+            col.gameObject.transform.position = spawnPoint.position;
+            col.gameObject.transform.rotation = spawnPoint.rotation;
+            col.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-		} 
-		//		else if (col.tag == "AI") {
-		//			col.gameObject.transform.position = spawnPoint.position;
-		//			col.gameObject.transform.rotation = spawnPoint.rotation;
-		//		}
-
-	}
+        }
+        //		else if (col.tag == "AI") {
+        //			col.gameObject.transform.position = spawnPoint.position;
+        //			col.gameObject.transform.rotation = spawnPoint.rotation;
+        //		}
+    }
+}
